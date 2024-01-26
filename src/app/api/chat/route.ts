aeaf1f69058c6fff8c3816f12f6203a5abdd988d
francostan <francostan98@hotmail.com>
2024-01-26 17:36:09 -0300

    terminado

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,51 +4,34 @@ import { NextResponse } from "next/server";
 const NPLKEY = process.env.NPL_KEY;
 
 export async function POST(request: Request) {
-  const { message, history } = await request.json();
+ const { message, history } = await request.json();
 
-  console.log(message, history);
-  console.log(NPLKEY);
+ console.log(message, history);
+ console.log(NPLKEY);
 
-  if (!NPLKEY) {
+ if (!NPLKEY) {
     return new NextResponse(JSON.stringify({ error: 'NLP_KEY is not defined' }), { status: 400 });
-  }
+ }
 
-  try {
+ try {
+    // Add delay before making the request
+    await new Promise(resolve => setTimeout(resolve, 4500));
+
     const client = new NLPCloudClient({
       model: 'chatdolphin',
       token: NPLKEY,
       gpu: true,
+   });
+
+    const response = await client.chatbot({
+      input: message,
+      context: 'This is a conversation with an AI assistant. Ai is helpful, creative, clever, and very friendly, her name is Valeria.',
+      history: history || [],
     });
-
-    let retries = 3;
-    let response;
-
-    while (retries > 0) {
-      try {
-        response = await client.chatbot({
-          input: message,
-          context: 'This is a conversation with an AI assistant. Ai is helpful, creative, clever, and very friendly, her name is Valeria.',
-          history: history || [],
-        });
-
-        break;
-      } catch (error) {
-        if (error.response && error.response.status === 429) {
-          const retryAfter = parseInt(error.response.headers['retry-after']) || 5;
-          console.log(`Rate limited. Retrying after ${retryAfter} seconds.`);
-          await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
-        } else {
-          throw error;
-        }
-      }
-
-      retries--;
-    }
-
     console.log(response.data);
     return new NextResponse(JSON.stringify(response.data), { status: 200 });
-  } catch (error) {
-    console.log(error);
-    return new NextResponse(JSON.stringify(error), { status: 500 });
-  }
+ } catch (error) {
+   console.log(error);
+    return new NextResponse(JSON.stringify(error), { status: 500});
+ }
 }
